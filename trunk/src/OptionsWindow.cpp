51da2113bcb1a07bb2071b8fb3546ad8d29a0576
rgw0094@gmail.com <rgw0094@gmail.com@d063b8e6-9d51-0410-bcaf-236e7d9db016>
2012-06-06 03:31:11 +0000

    Updated controls window

--- trunk/src/OptionsWindow.cpp
+++ trunk/src/OptionsWindow.cpp
@@ -9,7 +9,8 @@ extern SMH *smh;
 /**
  * Constructor
  */
-OptionsWindow::OptionsWindow() {
+OptionsWindow::OptionsWindow() 
+{
 	inputBox = new hgeRect();
 	soundVolumeSlider = new Slider(0, 0, 0, 100);
 	soundVolumeSlider->setValue(smh->soundManager->getSoundVolume());
@@ -23,30 +24,36 @@ OptionsWindow::OptionsWindow() {
 /**
  * Destructor
  */
-OptionsWindow::~OptionsWindow() {
+OptionsWindow::~OptionsWindow() 
+{
 	delete inputBox;
 	delete soundVolumeSlider;
 	delete musicVolumeSlider;
 	delete doneButton;
 }
 
-void OptionsWindow::setX(float newX) {
+void OptionsWindow::setX(float newX) 
+{
 	setWindowPosition(newX, windowY);
 }
 
-float OptionsWindow::getX() {
+float OptionsWindow::getX() 
+{
 	return windowX;
 }
 
-void OptionsWindow::setY(float newY) {
+void OptionsWindow::setY(float newY) 
+{
 	setWindowPosition(windowX, newY);
 }
 
-float OptionsWindow::getY() {
+float OptionsWindow::getY() 
+{
 	return windowY;
 }
 
-void OptionsWindow::setWindowPosition(float _x, float _y) {
+void OptionsWindow::setWindowPosition(float _x, float _y) 
+{
 	windowX = _x;
 	windowY = _y;
 
@@ -58,8 +65,8 @@ void OptionsWindow::setWindowPosition(float _x, float _y) {
 	doneButton->y = windowY + 340.0;
 }
 
-bool OptionsWindow::update(float dt) {
-
+bool OptionsWindow::update(float dt) 
+{
 	//Get mouse input
 	mouseX = smh->input->getMouseX();
 	mouseY = smh->input->getMouseY();
@@ -74,7 +81,8 @@ bool OptionsWindow::update(float dt) {
 
 	//Update done button
 	doneButton->update(dt);
-	if (doneButton->isClicked()) {
+	if (doneButton->isClicked()) 
+	{
 		//Make it so none of the buttons is in "Edit mode"
 		smh->input->setEditMode(-1);
 		
@@ -83,32 +91,30 @@ bool OptionsWindow::update(float dt) {
 	}
 
 	//Update input boxes
-	for (int col = 0; col < 2; col++) {
-		for (int row = 0; row < 5; row++) {
-		
-			currentInput = col*5 + row;
-			x = windowX + 40 + col*140.0;
-			y = windowY + 80 + row*80.0;
+	for (int i = 0; i < 10; i++)
+	{
+		x = windowX + 180;
+		y = windowY + 80 + i * 80.0;
 
-			//Listen for click to enable edit mode
-			inputBox->Set(x, y, x + 130.0, y + 30.0);
-			if (mousePressed && inputBox->TestPoint(mouseX, mouseY)) {
-				smh->input->setEditMode(currentInput);
-			}
+		//Listen for click to enable edit mode
+		inputBox->Set(x, y, x + 130.0, y + 30.0);
+		if (mousePressed && inputBox->TestPoint(mouseX, mouseY)) 
+		{
+			smh->input->setEditMode(currentInput);
+		}
 
-			//If the input is in edit mode, listen for the new input
-			if (smh->input->isEditModeEnabled(currentInput)) {
-				smh->input->listenForNewInput(currentInput);
-			}
-
+		//If the input is in edit mode, listen for the new input
+		if (smh->input->isEditModeEnabled(currentInput)) 
+		{
+			smh->input->listenForNewInput(currentInput);
 		}
 	}
 
 	return true;
 }
 
-void OptionsWindow::draw(float dt) {
-
+void OptionsWindow::draw(float dt) 
+{
 	//Draw background
 	smh->resources->GetSprite("optionsBackground")->Render(windowX, windowY);
 
@@ -130,39 +136,43 @@ void OptionsWindow::draw(float dt) {
 
 	doneButton->draw(dt);
 
+	smh->resources->GetFont("controls")->SetScale(0.7);
+	smh->resources->GetFont("controls")->SetColor(ARGB(255,255,255,255));
+	smh->resources->GetFont("controls")->Render(windowX + 40, windowY + 45, HGETEXT_LEFT, "Select Keyboard/Gamepad Inputs");
+
 	//Draw input boxes.
-	for (int col = 0; col < 2; col++) {
-		for (int row = 0; row < 5; row++) {
-		
-			currentInput = col*5 + row;
-			x = windowX + 40 + col*140.0;
-			y = windowY + 45 + row*80.0;
+	for (int i = 0; i < 10; i++)
+	{
+		x = windowX + 55;
+		y = windowY + 85 + i * 37.0;
 
-			//Input name
-			smh->resources->GetFont("controls")->SetScale(0.8);
-			smh->resources->GetFont("controls")->SetColor(ARGB(255,255,255,255));
-			smh->resources->GetFont("controls")->Render(x + 65.0, y, HGETEXT_CENTER, 
-				smh->input->getInputName(currentInput));
+		//Input name
+		smh->resources->GetFont("controls")->SetScale(0.8);
+		smh->resources->GetFont("controls")->SetColor(ARGB(255,255,255,255));
+		smh->resources->GetFont("controls")->Render(x, y, HGETEXT_LEFT, 
+			smh->input->getInputName(i));
 
-			//Input box
-			if (smh->input->isEditModeEnabled(currentInput)) {
-				smh->resources->GetSprite("selectedControlsBox")->Render(x,y+35);
-			} else {
-				smh->resources->GetSprite("controlsBox")->Render(x,y+35);
-			}
-
-			//Current setting
-			smh->resources->GetFont("controls")->SetColor(ARGB(255,255,255,255));
-			smh->resources->GetFont("controls")->SetScale(0.7);
-			smh->resources->GetFont("controls")->printf(x + 65.0, y + 40.0, HGETEXT_CENTER,
-					smh->input->getInputDescription(currentInput).c_str());
-			smh->resources->GetFont("controls")->SetColor(ARGB(255,0,0,0));
-
+		//Input box
+		if (smh->input->isEditModeEnabled(i)) 
+		{
+			smh->resources->GetSprite("selectedControlsBox")->Render(x+110,y-4);
 		}
+		else 
+		{
+			smh->resources->GetSprite("controlsBox")->Render(x+110,y-4);
+		}
+
+		//Current setting
+		smh->resources->GetFont("controls")->SetColor(ARGB(255,255,255,255));
+		smh->resources->GetFont("controls")->SetScale(0.7);
+		smh->resources->GetFont("controls")->printf(x + 175.0, y, HGETEXT_CENTER,
+				smh->input->getInputDescription(i).c_str());
+		smh->resources->GetFont("controls")->SetColor(ARGB(255,0,0,0));
 	}
 
 	//Draw the mouse
-	if (smh->input->isMouseInWindow()) {
+	if (smh->input->isMouseInWindow()) 
+	{
 		smh->resources->GetSprite("mouseCursor")->Render(mouseX, mouseY);
 	}
 
